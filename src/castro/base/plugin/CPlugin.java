7939e12004f427dd5994d20003cc1162ceb1198b
castro12321 <norbertkawinski@gmail.com>
2014-04-22 18:39:06 +0200

    baseinstance and vault moved upper to cPlugin

--- src/castro/base/plugin/CPlugin.java
+++ src/castro/base/plugin/CPlugin.java
@@ -17,16 +17,47 @@
 
 package castro.base.plugin;
 
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.ServicesManager;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
 
 public abstract class CPlugin extends CPluginMsg
 {
+	public static CPlugin baseinstance;
+	public static Economy economy;
+	public static Permission permissions;
+	
 	protected abstract void init();
 	
 	
 	@Override
 	protected void initBase()
 	{
+		baseinstance = this;
 		super.initBase();
+		registerVault();
+	}
+	
+	
+	private void registerVault()
+	{
+		try
+		{
+			ServicesManager services = getServer().getServicesManager();
+			RegisteredServiceProvider<Economy> economyProvider = services.getRegistration(net.milkbowl.vault.economy.Economy.class);
+			if (economyProvider != null && economy == null)
+				economy = economyProvider.getProvider();
+			RegisteredServiceProvider<Permission> permissionProvider = services.getRegistration(net.milkbowl.vault.permission.Permission.class);
+			if (permissionProvider != null && permissions == null)
+				permissions = permissionProvider.getProvider();
+		}
+		catch(Exception e)
+		{
+			log("Vault not present or something strange happened.");
+		}
 	}
 	
 	
@@ -34,12 +65,5 @@ public abstract class CPlugin extends CPluginMsg
 	{
 		initBase();
 		init();
-	}
-	
-	
-	@Override
-	protected CPlugin getBaseInstance()
-	{
-		return this;
 	}
 }
--- src/castro/base/plugin/CPluginBase.java
+++ src/castro/base/plugin/CPluginBase.java
@@ -17,45 +17,24 @@
 
 package castro.base.plugin;
 
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.permission.Permission;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.RegisteredServiceProvider;
-import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import castro.base.CCommandMgr;
 
 public abstract class CPluginBase extends JavaPlugin
 {
-	public static CPlugin baseinstance;
-	
 	private CCommandMgr commandMgr;
 	public FileConfiguration con; // config
-	public static Economy economy;
-	public static Permission permissions;
-	
 	
 	protected abstract CPluginSettings getSettings();
-	protected abstract CPlugin         getBaseInstance();
 	
 	
 	protected void initBase()
 	{
-		baseinstance = getBaseInstance();
-		
-		ServicesManager services = getServer().getServicesManager();
-		RegisteredServiceProvider<Economy> economyProvider = services.getRegistration(net.milkbowl.vault.economy.Economy.class);
-		if (economyProvider != null && economy == null)
-			economy = economyProvider.getProvider();
-		RegisteredServiceProvider<Permission> permissionProvider = services.getRegistration(net.milkbowl.vault.permission.Permission.class);
-		if (permissionProvider != null && permissions == null)
-			permissions = permissionProvider.getProvider();
-		
 		initBase(getSettings());
 	}
 	
